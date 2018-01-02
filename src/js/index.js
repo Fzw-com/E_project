3db6fde3cd90ab7efde764b9dcd25c81f7090a11
Fzw <2685316137@qq.com>
2018-01-02 11:38:49 +0800

    -m"更新头尾"

--- src/js/index.js
+++ src/js/index.js
@@ -22,5 +22,32 @@ jQuery(function($){
        'margin':'auto'
     })
     /*------------活动------------------------*/
+    /*------------------tab切换------------*/
+    // var $tab = $('.tab');
+    // var $tabItem = $tab.find('header div');
+    // var $tabContent = $tab.find('.content div');
+
+
+    // // 隐藏除第一个以外的图片
+    // $tabContent.slice(1).hide();
+    // // 默认第一个高亮
+    // $tabItem.first().addClass('active');
+
+
+    // $tab.on('mouseover','header > div',function(){
+    //     // 获取当前tab
+    //     // 添加高亮，出去其他高亮
+    //     $(this).addClass('active').siblings().removeClass('active');
+
+    //     // 获取当前索引值
+    //     var idx = $(this).index();console.log(idx)
+
+    //     // 切换当前图片
+    //     // $tabContent.eq(idx).show(1000).siblings().hide(600);
+
+    //     // 动画
+    //     // 滑动
+    //     $tabContent.eq(idx).slideDown().siblings().slideUp()
+    // });
 
 })
